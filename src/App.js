d11361ee726bdedd6be6a933ede824926a48eb17
kira <k-korni1ov@yandex.ru>
2023-07-12 10:11:37 +0400

    feat: setState

--- src/App.js
+++ src/App.js
@@ -4,23 +4,37 @@ import Header from './components/Header';
 import Image from './components/image';
 import logo from './img/anime-girl-rain.png'
   class  App extends React.Component{
-  
-  
-    helpText="HElp meee"
+    
+    constructor(props){
+      super(props)
+      this.state={
+        helpText:"HElp tetete",
+        userData:''
+      }
+      this.inputClick=this.inputClick.bind(this)
+    }
   
     render(){
       return (
         <div className="ff">
            <Header title="Shapka" />
           
-                        <h1>{this.helpText}</h1> <input placeholder={this.helpText} onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
-                    <p>{this.helpText==="HElp meee"?'YEs':'No'}</p>
+                        <h1>{this.state.helpText}</h1>
+                        <h2>{this.state.userData}</h2>
+                        
+                         <input placeholder={this.state.helpText}
+                         onChange={event=>this.setState({userData:event.target.value})}
+                         onClick={this.inputClick} onMouseEnter={this.mouseOver}/>
+                    <p>{this.state.helpText==="HElp meee"?'YEs':'No'}</p>
         <Image image={logo}/>
                     </div>
       )
     }
-    inputClick (){console.log("dfs")}
-    mouseOver (){console.log("gfds")}
+    inputClick (){
+      this.setState({helpText: "Changed"})
+      
+    }
+    mouseOver (){console.log("gfds")} 
   }
 
   export default App
